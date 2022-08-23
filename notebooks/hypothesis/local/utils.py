3a49bf713fff701cda67511608ea17c58dbf2045
Daniele Murer <80338169+daniele-murer@users.noreply.github.com>
2022-08-23 11:34:31 +0200

    Update utils.py to make it usable also in the distributed setting

--- notebooks/hypothesis/local/utils.py
+++ notebooks/hypothesis/local/utils.py
@@ -1,4 +1,5 @@
-import numpy as np, math, scipy
+import scipy
+
 
 def cohen_d(x, y):
     """Returns the Cohen's d estimator of the effect size.
@@ -17,26 +18,28 @@ def cohen_d(x, y):
     n_x = len(x)
     n_y = len(y)
     degrees_of_freedom = n_x + n_y - 2
-    pooled_std = (((n_x - 1) * np.var(x, ddof=1) + (n_y - 1) * np.var(y, ddof=1)) / degrees_of_freedom) ** 0.5
-    return (np.mean(x) - np.mean(y)) / pooled_std
+    pooled_std = (((n_x - 1) * x.std(ddof=1) ** 2 + (n_y - 1) * y.std(ddof=1) ** 2) / degrees_of_freedom) ** 0.5
+    return (x.mean() - y.mean()) / pooled_std
 
 
 def mean_test(x, mu):
-    """ Perform the mean test:
-        - Is the mean of the input (x) greater or equal to the expected mean (mu) ?
+    """Perform the mean test:
+        - Is the mean of the input (x) greater or equal to the expected mean (mu)?
         - Is it statistically significant?
 
-    See https://www.w3schools.com/statistics/statistics_hypothesis_testing_mean.php for more details
-    and the actual formula implemented in this funciton.
+    See https://www.w3schools.com/statistics/statistics_hypothesis_testing_mean.php for more
+    details and the actual formula implemented in this funciton.
 
     Args:
         x: An array-like containing samples drawn from a randomly selected population.
 
     Returns:
-        statistics: basically a measure proportional to the difference between the input (x) mean and the expected mean (mu)
-        p_value: level of significance
+        statistics: a metric proportional to the difference between the input (x) mean and the
+            expected mean (mu).
+        p_value: level of statistical significance.
     """
 
-    statistics = (x.mean() - mu) / (x.std() / math.sqrt(len(x)))
-    p_value =  1 - scipy.stats.t.cdf(statistics , len(x) - 1)
+    n_x = len(x)
+    statistics = (x.mean() - mu) * (n_x ** 0.5) / x.std(ddof=1)
+    p_value =  1 - scipy.stats.t.cdf(statistics, n_x - 1)
     return statistics, p_value
