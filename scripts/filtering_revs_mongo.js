1c2e46e285ce9b815365f0afc285010b9f1f3369
Daniele Murer <80338169+daniele-murer@users.noreply.github.com>
2022-09-02 12:57:44 +0200

    Make filtering_revs_mongo.js comply with JavaScript syntax

--- scripts/filtering_revs_mongo.js
+++ scripts/filtering_revs_mongo.js
@@ -1,39 +1,36 @@
 /*
-*****  CLEANING REVIEWS DATA WITH A COMLPEX MongoDB QUERY *****
-
-OBJECTIVE:
-It has been noticed that the data in our Data Source are not clean as expected, so some filtering has been applied to the following fields:
-    - negative playtime_after_review (playtime_forever - playtime_at_review)...how's possible?
-    - review with empty/null written text
-
-HOW TO RUN IT? 
-Execute in the same folder of this script the following command in terminal/bash
-    % mongosh < filtering_revs_mongo.js
+    * *** Cleaning of the small_reviews collection data stored in the MongoDB repository ***
+    * 
+    * The reviews records extracted from the data source contain incoherent data. This script:
+    *     - removes records with playtime_forever < playtime_at_review (it should be impossible);
+    *     - removes records with null/empty text reviews.
+    * 
+    * How to run this script:
+    * $ mongosh -f filtering_revs_mongo.js
 */
 
+// Connect to the MongoDB instance and select the final_project database
+db = connect('mongodb://localhost/final_project');
 
-// Move to final project MongoDB
-use final_project
+// Remove the small_reviews_filtered collection if it already exists
+db.small_reviews_filtered.drop({});
 
-// Remove small_reviews_filtered collection if already exists
-db.small_reviews_filtered.drop({})
+// Create the collection small_reviews_filtered with the filtered data from small_reviews
+db.small_reviews.aggregate([
+    {
+        $match: {
+            // Combine multiple checks on relevant fields
+            $and: [
+                // Filter out the null/empty text reviews
+                {review: {$ne: null}},
+                {review: {$ne: ''}},
 
-// Create a new collection named small_reviews_filtered, filtering the data of the small_reviews collection
-db.small_reviews.aggregate(
-    [
-        {
-            $match: {
-                // Combine multiple checks on chosen fields
-                $and:
-                    [
-                        // Filter on empty/null text reviews
-                        { 'review': { $ne: null } }, { 'review': { $ne: '' } },
-                        // Filter on negative playtime_after_review
-                        { $expr: { $gte: ['$playtime_forever', '$playtime_at_review'] } }
-                    ]
-            }
-        },
-    // Store the filtering result in the small_reviews_filtered collections
-    { $out: 'small_reviews_filtered' }
-    ]
-)
+                // Filter out the reviews with a negative playtime after review
+                {$expr: {$gte: ['$playtime_forever', '$playtime_at_review']}}
+            ]
+        }
+    },
+
+    // Store the result of the filtering in the small_reviews_filtered collection
+    {$out: 'small_reviews_filtered'}
+]);
